adf50ba5938000952df5754c9f92d59ab2416177
mushonnip <mushonnipabu@gmail.com>
2022-06-20 21:48:02 +0700

    init

---
+++ cff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /root
+echo -e "domain=sumbawa.me" >> /root/mail.conf
+echo -e "email=paoandest@gmail.com" >> /root/mail.conf
+echo -e "key=1d158d0efc4eef787222cefff0b6d20981462" >> /root/mail.conf
+clear
+echo "Done"
+echo "Your ID Cloudflare"
+echo -e "==============================="
+echo "DOMAIN         : sumbawa.me"
+echo "Email          : paoandest@gmail.com"
+echo "Api Key        : 1d158d0efc4eef787222cefff0b6d20981462"
+echo -e "==============================="
